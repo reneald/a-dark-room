2f4e08dddb0563788d1f2346cadae6c8b6cb8867
reneald <reneald@protonmail.com>
2022-11-03 20:05:13 +0100

    add banner and helper class

---
+++ src/main/java/be/leanderonline/adarkroom/util/Helper.java
@@ -0,0 +1,12 @@
+package be.leanderonline.adarkroom.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Helper {
+    public static final String TITLE = " ____    ____  ____  ____  _  __   ____  ____  ____  _     \n" +
+            "/  _ \\  /  _ \\/  _ \\/  __\\/ |/ /  /  __\\/  _ \\/  _ \\/ \\__/|\n" +
+            "| / \\|  | | \\|| / \\||  \\/||   /   |  \\/|| / \\|| / \\|| |\\/||\n" +
+            "| |-||  | |_/|| |-|||    /|   \\   |    /| \\_/|| \\_/|| |  ||\n" +
+            "\\_/ \\|  \\____/\\_/ \\|\\_/\\_\\\\_|\\_\\  \\_/\\_\\\\____/\\____/\\_/  \\|";
+}
